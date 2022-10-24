59b6b92bd7412d446d360fc7e3e13086d7c2831a
Faizan Ali <faizanscorpioo@gmail.com>
2022-10-24 17:11:31 +0500

    CopyToClipboard-with-javaScript
    CopyToClipboard-with-javaScript code base project work updated

---
+++ clicktoCopyToClipBoard______with_js____.js
@@ -0,0 +1,17 @@
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+
+function copyToClipboard(text) {
+   const elem = document.createElement('textarea');
+   elem.value = text;
+   document.body.appendChild(elem);
+   elem.select();
+   document.execCommand('copy');
+   document.body.removeChild(elem);
+}
